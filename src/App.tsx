074847bd27ad9501cc0341091e884c8313566f61
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-07-05 21:48:17 -0400

    Add prompt capability

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [prompt, setPrompt] = useState<string>('');
+  const [result, setResult] = useState<string>('');
+
+  async function doPrompt(prompt: string) {
+    const session = await window.ai.createTextSession();
+    const result =  await session.prompt(prompt);
+    return result;
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <div className="user-input-area">
+        <input
+          type="text"
+          placeholder="What is 2 + 2?"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+        />
+        <button
+          onClick={async () => {
+            const response = await doPrompt(prompt);
+            setResult(response);
+          }}
+        >
+          Prompt
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {result ? <p>{result}</p> : null}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
