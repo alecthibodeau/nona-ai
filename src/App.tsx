04d42c5a6ec4cea1dda61012c768e3eb58af575a
Alec Thibodeau <39539563+alecthibodeau@users.noreply.github.com>
2024-07-06 21:05:26 -0400

    Add UserInputForm component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,14 @@
-import { useState } from 'react';
+/* Components */
+import UserInputForm from './components/UserInputForm';
+
+/* Styles */
 import './App.css';
 
 function App() {
-  const [prompt, setPrompt] = useState<string>('');
-  const [result, setResult] = useState<string>('');
-
-  async function doPrompt(prompt: string) {
-    const session = await window.ai.createTextSession();
-    const result =  await session.prompt(prompt);
-    return result;
-  }
-
   return (
-    <>
-      <div className="user-input-area">
-        <input
-          type="text"
-          placeholder="What is 2 + 2?"
-          value={prompt}
-          onChange={(e) => setPrompt(e.target.value)}
-        />
-        <button
-          onClick={async () => {
-            const response = await doPrompt(prompt);
-            setResult(response);
-          }}
-        >
-          Prompt
-        </button>
-      </div>
-      {result ? <p>{result}</p> : null}
-    </>
+    <div className="app">
+      <UserInputForm />
+    </div>
   );
 }
 
